cd36095a0e38e45798f3e5c2642e6cb5a0b7ea22
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-04 10:17:07 +0200

    Add files via upload
    tests for configure-wardrobe.js

---
+++ configure-wardrobe.test.js
@@ -0,0 +1,13 @@
+const {combination} = require('./configure-wardrobe.js')
+
+describe('combination', ()=>{
+    describe('exact fit', () =>{
+        
+        let sum; 
+        sum += combo[[i]];
+      
+      [sum == wall].forEach(combo[i]);{ //=> ?
+          it('should return sum = 250')
+        };
+    });
+});
