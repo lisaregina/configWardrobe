e602e11bca25ec3eddb4f5c1517984cc8507d873
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-04 11:37:27 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # configWardrobe
 University Project
+ 
+You have to create a program to get all combinations of wardrobe elements (sizes: 50cm, 75cm, 100cm, 120cm), which fit in the wall size of 250cm
---
+++ configure-wardrobe.test.js
@@ -0,0 +1,17 @@
+    const sum; 
+    sum += combo[[i]];
+    
+        [sum == wall].forEach(combo[i]);{ //=> ?
+        it('should return sum = 250')
+    };
+
+describe("configure-wardrobe", () =>{
+    it("should return sum == wall", () =>{
+        console.log(combo)
+    })
+});
+
+
+    
+
+
