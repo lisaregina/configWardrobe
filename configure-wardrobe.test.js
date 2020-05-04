131ed662d0db7a86bd3fe70a98d6d657c021fdeb
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-04 11:47:38 +0200

    Add files via upload

--- configure-wardrobe.test.js
+++ configure-wardrobe.test.js
@@ -1,13 +1,7 @@
-    const sum; 
-    sum += combo[[i]];
-    
-        [sum == wall].forEach(combo[i]);{ //=> ?
-        it('should return sum = 250')
-    };
-
 describe("configure-wardrobe", () =>{
     it("should return sum == wall", () =>{
-        console.log(combo)
+        expect(combination()).toBe(0)
+        
     })
 });
 
