0c37d833181184adb061db63ce72191a2be2e1ca
lisaregina <64302527+lisaregina@users.noreply.github.com>
2020-05-04 11:36:07 +0200

    Add files via upload
    Überarbeitung Test, aber immernoch wahrscheinlich falsch

--- configure-wardrobe.test.js
+++ configure-wardrobe.test.js
@@ -1,14 +1,17 @@
-const {combination} = require('./configure-wardrobe.js')
+    const sum; 
+    sum += combo[[i]];
+    
+        [sum == wall].forEach(combo[i]);{ //=> ?
+        it('should return sum = 250')
+    };
 
-describe('combination', ()=>{
-    describe('exact fit', () =>{
-        
-        let sum; 
-        sum += combo[[i]];
-      
-      [sum == wall].forEach(combo[i]);{ //=> eigentlich müsste da noch ein => sein, aber zeigt mir dadurch ein Problem an?!
-          it('should return sum = 250')
-        };
-    });
+describe("configure-wardrobe", () =>{
+    it("should return sum == wall", () =>{
+        console.log(combo)
+    })
 });
-//wahrscheinlich total falsch
+
+
+    
+
+
